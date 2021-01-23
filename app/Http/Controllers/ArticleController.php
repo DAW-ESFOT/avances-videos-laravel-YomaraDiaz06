fd5afcbdf80e72b354a46353739eb78d458be880
YomaraDiaz06 <Yomara.diaz@epn.edu.ec>
2021-01-23 17:04:32 -0500

    #15 validation-article

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -5,12 +5,20 @@ namespace App\Http\Controllers;
 use App\Article;
 use App\Http\Resources\ArticleCollection;
 use App\Http\Resources\Article as ArticleResource;
-
 use Illuminate\Http\Request;
 
 
 class ArticleController extends Controller
 {
+    private static $rules =[
+        'tittle' => 'required|string|unique:articles|max:255',
+        'body' => 'required',
+        ];
+    private static $messages=[
+        'requerid' => 'el campo :attribute es obligatorio',
+        'body.requerid' =>'el body no es valido',
+        ];
+
     public function index()
     {
         return new ArticleCollection(Article::pagination());
@@ -21,6 +29,9 @@ class ArticleController extends Controller
     }
     public function store(Request $request)
     {
+
+        $request->validate(self::$rules,self::$messages);
+
         $article = Article::create($request->all());
         return response()->json($article, 201);
     }
